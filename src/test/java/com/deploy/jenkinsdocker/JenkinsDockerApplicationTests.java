9e42ada87a2e6a5b7abc1c73c3133a2acd808923
sourcesyy <15533330802@163.com>
2020-05-19 17:16:31 +0800

    首次提交

---
+++ src/test/java/com/deploy/jenkinsdocker/JenkinsDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.jenkinsdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
