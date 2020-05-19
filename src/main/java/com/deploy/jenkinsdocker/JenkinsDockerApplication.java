39eba63d283cb019dad89752bdc01250211773bd
sourcesyy <15533330802@163.com>
2020-05-19 17:19:24 +0800

    提交代码

--- src/main/java/com/deploy/jenkinsdocker/JenkinsDockerApplication.java
+++ src/main/java/com/deploy/jenkinsdocker/JenkinsDockerApplication.java
@@ -2,12 +2,20 @@ package com.deploy.jenkinsdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsDockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDockerApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(){
+		return "hello Deploy with jenkins and docker";
 	}
 
 }
